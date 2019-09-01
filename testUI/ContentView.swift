6139ddbcfb3648d7744365ea393324b4efa839eb
shopback <boring.hsiao@shopback.com>
2019-09-01 19:12:09 +0800

    Initial Commit

---
+++ testUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testUI
+//
+//  Created by ShopBack on 2019/9/1.
+//  Copyright © 2019 ShopBack. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
